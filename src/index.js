d1d8fdc9054d1c35a7eef7245f32d549da2b8614
Michał Murawski <m.a.murawski@wp.pl>
2022-11-16 23:39:47 +0100

    add axios and notiflix package

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './css/styles.css';
+import axios from 'axios';
+// import Notiflix from 'notiflix';
+
+axios
+  .get('')
+  .then(response  => {
+    // handle success
+    console.log(response);
+  })
+  .catch(error => {
+    // handle error
+    console.log(error);
+  })
+  .finally(() => {
+    // always executed
+  });
